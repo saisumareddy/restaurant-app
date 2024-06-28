0a09ed1bd5057054c07c3d7ecd2632529e2a8814
“saisumareddygit config --global user.email “saisumareddy@gmail.com <saisumareddy.svr@gmail.com>
2024-06-28 16:15:04 +0530

    improved responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isSmallScreen: boolean = false;
+  sidenavOpened: boolean = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isSmallScreen = result.matches;
+        if (this.isSmallScreen) {
+          this.sidenavOpened = false;
+        } else {
+          this.sidenavOpened = true;
+        }
+      });
+  }
+
+  toggleSidenav() {
+    this.sidenavOpened = !this.sidenavOpened;
+  }
 }
